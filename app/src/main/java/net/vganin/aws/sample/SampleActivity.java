b999e3771080ffc24da371ee9f56a463a040055f
vganin <vsevolod.ganin@gmail.com>
2016-01-28 20:48:23 +0300

    Remove useless imports

--- app/src/main/java/net/vganin/aws/sample/SampleActivity.java
+++ app/src/main/java/net/vganin/aws/sample/SampleActivity.java
@@ -1,8 +1,6 @@
 package net.vganin.aws.sample;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.format.DateUtils;
